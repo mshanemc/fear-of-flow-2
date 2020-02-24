adcac192879bd24bd3e1aa08c49082221faf73c4
mshanemc <shane.mclaughlin@salesforce.com>
2020-02-24 08:41:09 -0600

    package + perm assign

---
+++ orgInit.sh
@@ -0,0 +1,5 @@
+sfdx force:org:create -f config/project-scratch-def.json -d 7 -s
+sfdx force:source:push
+sfdx force:package:install --package 04t1U000007sgLo -w 20 -r
+sfdx force:user:permset:assign -n "Flow_HoT_Perm_Set"
+sfdx force:org:open
