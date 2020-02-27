c094733936f29bf5f978b7898e8548447328511b
mshanemc <shane.mclaughlin@salesforce.com>
2020-02-27 17:34:27 -0600

    package without permset

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,6 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 7 -s
 sfdx force:source:push
-sfdx force:package:install --package 04t1U000007sgLt -w 20 -r
+sfdx force:package:install --package 04t1U000007sgeT -w 20 -r
 sfdx force:user:permset:assign -n Run_Flows
 sfdx force:user:create --definitionfile config/userDef/testUser.json
 sfdx shane:user:permset:assign --firstname Test --lastname User --name Run_Flows
