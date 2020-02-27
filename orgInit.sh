1169450c3a59d6a898e5d664559978bb0bcacd59
mshanemc <shane.mclaughlin@salesforce.com>
2020-02-27 14:34:12 -0600

    flows permission set

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,9 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 7 -s
 sfdx force:source:push
 sfdx force:package:install --package 04t1U000007sgLt -w 20 -r
-sfdx force:user:permset:assign -n "Flow_HoT_Perm_Set"
+sfdx force:user:permset:assign -n Flow_HoT_Perm_Set
+sfdx force:user:permset:assign -n Run_Flows
 sfdx force:user:create --definitionfile config/userDef/testUser.json
 sfdx shane:user:permset:assign --firstname Test --lastname User --name Flow_HoT_Perm_Set
+sfdx shane:user:permset:assign --firstname Test --lastname User --name Run_Flows
 sfdx force:org:open
